c2ab9dcd1e4dea035fb46bbd477580f5ba0e7fb6
smrkem <smrkem@gmail.com>
2014-02-28 15:00:47 -0500

    units.php

--- application/controllers/units.php
+++ application/controllers/units.php
@@ -14,24 +14,28 @@ class Units extends MY_Controller{
             $page->show();
 	}
         
-        public function save_unit(){
-            if(!$this->user || $this->user->status()!=='active'){
-                header("Location:".base_url());
-                exit();
-            }
-            $this->load->model('units_m');
-            $this->load->model('materials_m');
-            
-            if(isset($_POST['add_unit'])){
-                $this->units_m->save_unit($_POST);
-                header('Location:'.base_url());
-                exit();
-            }
-            
-            $page = new Page('unit');
-            $page->Data('content_types',$this->materials_m->get_content_types());
-            $page->content('save_unit-v');
-            $page->show();
+    public function save_unit() {
+        $this->load->model('units_m');
+        $this->load->model('materials_m');
+
+        if (!$this->user || $this->user->status() !== 'active') {
+            header("Location:" . base_url());
+            exit();
         }
+
+        if (isset($_POST['add_unit'])) {  // if user has clicked the Save button
+            if ($this->chk_form()) {      // if the form has been validated
+                $this->extract_id();    // call function to extract video IDs
+                $this->units_m->save_unit($_POST);
+                header('Location:' . base_url());
+                exit();
+            }
+        }
+
+        $page = new Page('unit');
+        $page->Data('content_types', $this->materials_m->get_content_types());
+        $page->content('save_unit-v');
+        $page->show();
+    }
 	
 }
